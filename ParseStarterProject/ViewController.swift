c8ad5246a71e66959e6feb3e65008c66390f1096
Regular User <lynnk@Lynns-MacBook-Pro.local>
2015-11-03 08:25:12 -0800

    Sortogram project

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -10,15 +10,92 @@
 import UIKit
 import Parse
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func presentImagePickerButton(sender: UIButton) {
+        
+        print("Button selected..")
+        
+    
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            self.presentActionSheet()
+        } else {
+            self.presentImagePickerFor(.PhotoLibrary)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.setupAppearance()
+        
+  // Testing the parse backend (parse.com)
+  /*
+        
+        let testObject = PFObject(className: "Status")
+        testObject["text"] = "This is a new text."
+        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            print("Object has been saved.")
+*/
+        }
+        
+
+    
+    func setupAppearance() {
+        self.imageView.layer.borderColor = UIColor.lightGrayColor().CGColor
+        self.imageView.layer.borderWidth = 0.6
     }
+    
+    func presentActionSheet() {
+        
+                let alertController = UIAlertController(title: "", message: "Please choose your source.", preferredStyle: .ActionSheet)
+                
+                let cameraAction = UIAlertAction(title: "Camera", style: UIAlertActionStyle.Default) { (action) -> Void in
+                    self.presentImagePickerFor(.Camera)
+                }
+                
+                let photoLibraryAction = UIAlertAction(title: "Photo Library", style: UIAlertActionStyle.Default) { (action) -> Void in
+                    self.presentImagePickerFor(.PhotoLibrary)
+                }
+                
+                let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+                
+                alertController.addAction(cameraAction)
+                alertController.addAction(photoLibraryAction)
+                alertController.addAction(cancelAction)
+                
+                self.presentViewController(alertController, animated: true, completion: nil)
+            }
+    
+    func presentImagePickerFor(sourceType: UIImagePickerControllerSourceType) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = sourceType
+        imagePickerController.delegate = self
+        self.presentViewController(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func presentAlertView() {
+        let alertController = UIAlertController(title: "", message: "Image successfully uploaded.", preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+        alertController.addAction(okAction)
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    // MARK: UIImagePickerController Delegate
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        self.imageView.image = image
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+   
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
